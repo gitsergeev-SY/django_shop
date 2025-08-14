32c625bbe9841ce0c9a4f3e7cba3418d0a9fdf4c
Sergey <sergeev191419@gmail.com>
2025-08-14 18:12:05 +0300

    second comit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ['name', 'slug']
+  prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product) 
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ['name', 'category', 'slug', 'image', 'price', 'description', 'available', 'created_at', 'updated_at']
+  list_filter = ['available', 'created_at', 'updated_at', 'category']
+  list_editable = ['available', 'price']
+  prepopulated_fields = {'slug': ('name',)}
--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+   name = models.CharField(max_length=100, db_index=True)
+   slug = models.SlugField(max_length=100, unique=True)
+
+   def __str__(self):
+       return self.name
+   
+   class Meta:
+      ordering = ['name',]
+      verbose_name = 'category'
+      verbose_name_plural = 'categories'
+
+class Product(models.Model):
+   category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+   name = models.CharField(max_length=100, db_index=True)
+   slug = models.SlugField(max_length=100, unique=True)
+   image = models.ImageField(upload_to='products/%Y/%m/%d')
+   price = models.DecimalField(max_digits=20, decimal_places=2)
+   description = models.TextField(blank=True)
+   available = models.BooleanField(default=True)
+   created_at = models.DateTimeField(auto_now_add=True, null=True) 
+   updated_at = models.DateTimeField(auto_now=True)
+   
+   class Meta:
+      ordering = ['name',] 
+    
+   def __str__(self):
+       return self.name
+   
+ 
+   
